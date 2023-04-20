e1b657f3d244fa3a72289825b9c566ca40abe5ec
Santosh Timilisina <santos.timil@gmail.com>
2023-04-20 16:20:49 +0545

    spring-security-using-inmemmory-users

---
+++ src/main/java/com/example/springs/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.springs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withUsername("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles("USER")
+                        .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/home", "/","/login").permitAll()
+                                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                                .requestMatchers("/user/**").hasAnyRole("USER")
+                                .anyRequest().authenticated())
+                .formLogin((form) ->
+                        form.loginPage("/login").permitAll()
+                ).logout((logout) ->
+                        logout.permitAll());
+        return http.build();
+    }
+
+
+}

8ddc493ed5cdc83a4e577da1c2dcae70f1f07fed
Santosh Timilisina <santos.timil@gmail.com>
2023-04-27 23:07:06 +0545

    using authenticationmanagerbuilderto confiugre inmemmory user

--- src/main/java/com/example/springs/config/WebSecurityConfig.java
+++ src/main/java/com/example/springs/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.springs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,21 +21,21 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withUsername("user")
-                        .password(passwordEncoder().encode("user"))
-                        .roles("USER")
-                        .build();
-
-        UserDetails admin = User
-                .withUsername("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withUsername("user")
+//                        .password(passwordEncoder().encode("user"))
+//                        .roles("USER")
+//                        .build();
+//
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -48,6 +50,27 @@ public class WebSecurityConfig {
                 ).logout((logout) ->
                         logout.permitAll());
         return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        InMemoryUserDetailsManager newUser = new InMemoryUserDetailsManager();
+        UserDetails user =
+                User.withUsername("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles("USER")
+                        .build();
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        newUser.createUser(user);
+        authenticationManagerBuilder.inMemoryAuthentication().withUser(user);
+        authenticationManagerBuilder.inMemoryAuthentication().withUser(admin);
+        return authenticationManagerBuilder.build();
     }
 
 
